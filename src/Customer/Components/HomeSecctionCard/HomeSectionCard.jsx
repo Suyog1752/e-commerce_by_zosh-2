1606234aa74c8cf2093dff5391d4e5857977e1a4
Suyog <Suyog@123>
2023-11-27 11:14:30 +0530

    e-commerce

---
+++ src/Customer/Components/HomeSecctionCard/HomeSectionCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeSectionCard = () => {
+  return (
+    <div className='cursore-pointer flrx-col items-center bg-white rounded-lg shadow-lg 
+    overflow-hidden w-[150rem] mx-3'>
+      <div className='h-[13rem] w-[10rem]'></div>
+
+    </div>
+  )
+}
+
+export default HomeSectionCard
---
+++ src/Customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCrosel from '../../Components/HomeCarosel/MainCrosel'
+
+const HomePage = () => {
+  return (
+    <div>
+<MainCrosel/>
+  <div>
+    other section
+  </div>
+    </div>
+  )
+}
+
+export default HomePage
